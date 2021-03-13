8b24104fc61dc9eeaec8859e497216ba142b5293
Timothy Baldridge <tbaldridge@gmail.com>
2021-03-13 11:09:56 -0700

    initial commit

---
+++ SynthNPCsWithFaces/Extensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Mutagen.Bethesda;
+
+namespace SynthNPCsWithFaces
+{
+    public static class Extensions
+    {
+        public static HashSet<string> StockESMs = new(comparer: StringComparer.InvariantCultureIgnoreCase)
+        {
+            "Skyrim.esm",
+            "Update.esm",
+            "Dawnguard.esm",
+            "HearthFires.esm",
+            "Dragonborn.esm"
+        };
+        public static IEnumerable<T> NoStockRecords<T>(this IEnumerable<T> records) where T : IMajorRecordGetter
+        {
+            Console.WriteLine($"{records.First().FormKey.ModKey.Name} {records.First().FormKey.ModKey.FileName} {records.First().FormKey.ModKey.Type}");
+            return records.Where(r => !StockESMs.Contains(r.FormKey.ModKey.FileName));
+        }
+    }
+}
---
+++ SynthNPCsWithFaces/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Mutagen.Bethesda;
+using Mutagen.Bethesda.Skyrim;
+using Mutagen.Bethesda.Synthesis;
+
+namespace SynthNPCsWithFaces
+{
+    class Program
+    {
+        public static async Task<int> Main(string[] args)
+        {
+            return await SynthesisPipeline.Instance.AddPatch<ISkyrimMod, ISkyrimModGetter>(RunPatch).Run(args, new RunPreferences()
+            {
+                ActionsForEmptyArgs = new RunDefaultPatcher()
+                {
+                    IdentifyingModKey = "HalgariSoulGemDisenchantment.esp",
+                    TargetRelease = GameRelease.SkyrimSE,
+                }
+            });
+
+
+        }
+        
+        public static void RunPatch(IPatcherState<ISkyrimMod, ISkyrimModGetter> state)
+        {
+            var races = state.LoadOrder.PriorityOrder.Race()
+                .WinningOverrides()
+                .Where(race => race.Flags.HasFlag(Race.Flag.FaceGenHead))
+                .Where(race => !race.Flags.HasFlag(Race.Flag.Child))
+                .ToDictionary(race => race.FormKey);
+            
+            Console.WriteLine($"Found {races.Count} races");
+
+            var npcs = state.LoadOrder.PriorityOrder.Npc()
+                .WinningOverrides()
+                .Where(npc =>
+                    (!npc.Template.IsNull ||
+                     !npc.Configuration.TemplateFlags.HasFlag(NpcConfiguration.TemplateFlag.Traits))
+                    && races.ContainsKey(npc.Race.FormKey))
+                .ToArray();
+            
+            Console.WriteLine($"Found {npcs.Length} NPCs");
+                
+            foreach (var npc in npcs) 
+                state.PatchMod.Npcs.Add(npc.DeepCopy());
+
+            var headParts = state.LoadOrder.PriorityOrder.HeadPart()
+                .WinningOverrides()
+                .NoStockRecords()
+                .ToArray();
+
+            Console.WriteLine($"Found {headParts.Length} Head Parts");
+            
+            foreach (var headPart in headParts) 
+                state.PatchMod.HeadParts.Add(headPart.DeepCopy());
+            
+            var colors = state.LoadOrder.PriorityOrder.ColorRecord()
+                .WinningOverrides()
+                .NoStockRecords()
+                .ToArray();
+
+            Console.WriteLine($"Found {colors.Length} Colors");
+            
+            foreach (var color in colors) 
+                state.PatchMod.Colors.Add(color.DeepCopy());
+
+        }
+    }
+}
